cda5ef0e74e0ccf71e1253c391db20b4cdb6e45a
Manav Zadafiya <132845969+Mz004@users.noreply.github.com>
2024-09-22 00:15:13 -0400

    Update config.js

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,8 +1,8 @@
 // src/config.js
 
 const config = {
-    baseURL: 'http://localhost:8080',
-    // baseURL: 'https://library-app-f2pc.onrender.com',
+    // baseURL: 'http://localhost:8080',
+    baseURL: 'https://library-app-f2pc.onrender.com',
   };
   
   export default config;
